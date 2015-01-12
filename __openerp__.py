3c5ff56c693f425b33f18d6097221a46bd00d836
gfcapalbo <giovanni@therp.nl>
2015-01-12 16:00:25 +0100

    [ADD] basic website module skeleton

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+'name': "website_therp",
+'description': """
+Website module for educational purposes
+""",
+'depends': ['base',
+            'website',
+           ],
+'data': [
+        'templates/templates.xml',
+        'data/data.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        ],
+}
