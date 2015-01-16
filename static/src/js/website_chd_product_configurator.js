8db6ee0bf03c3ca079a0b8047656a022e7fb21f4
gfcapalbo <giovanni@therp.nl>
2015-01-16 18:58:33 +0100

    [fix] add javascript file

---
+++ static/src/js/website_chd_product_configurator.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+$('.product_selection').each(function () {
+    var product_selection = this;
+    console.log( "ready!" );
+    $(product_selection).on('click', '.product_selection', function () {
+    	console.log("product id " + this.value + " now access the complete model");
+    	console.log( "postdebug" );
+    	openerp.jsonRpc("/chd_init/get_options", 'call', {
+             'id': this.value,
+    		  })
+                 console.log("json data returned from openerp");
+                 console.log(data);
+    });
+});
+});
